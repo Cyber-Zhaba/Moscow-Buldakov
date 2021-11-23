e9fb18ce95bddc623fbcb3c96365c3e2f204ca81
CyberZhaba <20096ninja@gmail.com>
2021-11-23 12:43:18 +0300

    Not only yellow circles

--- first.py
+++ first.py
@@ -28,7 +28,7 @@ class MyWidget(QMainWindow):
             qp.end()
 
     def draw_flag(self, qp):
-        qp.setBrush(QColor(255, 255, 0))
+        qp.setBrush(QColor(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
         d = random.randint(10, 350)
         qp.drawEllipse(random.randint(0, 300), random.randint(0, 300), d, d)
 
